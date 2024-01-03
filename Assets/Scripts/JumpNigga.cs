8814ecc41b4ddde41aef9a282c060e2149bf3459
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2024-01-03 22:20:27 +0100

    nice

--- Assets/Scripts/JumpNigga.cs
+++ Assets/Scripts/JumpNigga.cs
@@ -4,23 +4,31 @@ using UnityEngine;
 
 public class JumpNigga : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public Rigidbody2D rb;
+    public float speed = 3;
     public float jumpAmount = 10;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        bool isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.AddForce(Vector2.left * 5, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Vector2.right * speed);
         }
     }
 }
